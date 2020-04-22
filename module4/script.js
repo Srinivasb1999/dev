9f746b38864488a96265d50f491bcec466083754
Srinivas <srinivasb1999@gmail.com>
2020-04-22 17:05:53 +0530

    updates

--- module4/script.js
+++ module4/script.js
@@ -66,3 +66,15 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })(window);
+
+
+// var names=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+//         for(var i=0; i<names.length;i++){
+//             if(names[i][0]=="j" || names[i][0]=="J"){
+//                 console.log("Good Bye "+ names[i]);
+//             }
+//             else{
+//                 console.log("Hello "+ names[i]);
+//             }
+            
+//         }
